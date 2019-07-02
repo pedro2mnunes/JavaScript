c88f69db3b947505274f110172ad7c54062b0cb5
Pedro Miguel Martinho Nunes <44981871+pedro2mnunes@users.noreply.github.com>
2019-07-02 22:05:03 +0100

    Introduce BMI Calculator

--- StringConcatenation.js
+++ StringConcatenation.js
@@ -4,3 +4,26 @@ alert(msg);
 var  tweet = ("Please enter your text", "MAX 140 char");
 prompt (tweet);
 window.alert("You have written " + tweet.length + " characters, you have " + -(140-tweet.length) + " characters left.");
+function getMilk() {
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+    console.log("enterHouse");
+}
+
+function bmiCalculator(weight,height){
+return Math.round (weight/(height*height))}
