d3053853e43087a662515c8e46d696e638d8d05f
Pedro Miguel Martinho Nunes <44981871+pedro2mnunes@users.noreply.github.com>
2019-07-02 20:25:46 +0100

    Insert getMilk function.

--- StringConcatenation.js
+++ StringConcatenation.js
@@ -3,3 +3,4 @@ var msg =" Hello " + name + "!";
 alert(msg);
 var  tweet = ("Please enter your text", "MAX 140 char");
 prompt (tweet);
+window.alert("You have written " + tweet.length + " characters, you have " + -(140-tweet.length) + " characters left.");
